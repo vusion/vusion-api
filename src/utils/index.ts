428d5781109fb1ca629b3f0714f28fe1c43f0f59
Rainfore <rainforest92@126.com>
2019-09-18 23:43:59 +0800

    :bug: Fix subDir name

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const kebab2Camel = (name: string) => name.replace(/(?:^|-)([a-z])/g, (m, $1) => $1.toUpperCase());
+export const kebab2Camel = (name: string) => name.replace(/(?:^|-)([a-zA-Z0-9])/g, (m, $1) => $1.toUpperCase());
 
-export const Camel2kebab = (name: string) => name.replace(/([A-Z])/g, (m, $1, offset) => (offset ? '-' : '') + $1.toLowerCase());
+export const Camel2kebab = (name: string) => name.replace(/([A-Z]|[0-9]+)/g, (m, $1, offset) => (offset ? '-' : '') + $1.toLowerCase());
 
