cf6ec305fe1bfdb014126832ac48789522f32017
Forrest <rainforest92@126.com>
2020-05-23 16:16:35 +0800

    :art: Add justExec

--- out/cli/process.js
+++ out/cli/process.js
@@ -11,13 +11,27 @@ const logger = require("./logger");
  */
 function execSync(...args) {
     const command = args.join(' ');
+    return child_process_1.spawnSync(command, { shell: true, stdio: 'inherit' });
+}
+exports.execSync = execSync;
+;
+/**
+ * 使用 spawnSync 的 shell inherit 模式，直接对接主进程的 stdio
+ * 如果 code 非 0，则直接结束
+ * @param args 命令参数，每一项可以为字符串或是字符串数组
+ * @example
+ * execSync('rm', '-rf', 'node_modules')
+ * execSync('git clone', 'xxx')
+ */
+function justExecSync(...args) {
+    const command = args.join(' ');
     const result = child_process_1.spawnSync(command, { shell: true, stdio: 'inherit' });
     if (result.status) {
         logger.error(String(result.stderr || result.stdout));
         process.exit(1);
     }
 }
-exports.execSync = execSync;
+exports.justExecSync = justExecSync;
 ;
 /**
  * 使用 spawn 的 shell inherit 模式，直接对接主进程的 stdio
--- src/cli/process.ts
+++ src/cli/process.ts
@@ -10,6 +10,19 @@ import * as logger from './logger';
  */
 export function execSync(...args: Array<string>) {
     const command = args.join(' ');
+    return spawnSync(command, { shell: true, stdio: 'inherit' });
+};
+
+/**
+ * 使用 spawnSync 的 shell inherit 模式，直接对接主进程的 stdio
+ * 如果 code 非 0，则直接结束
+ * @param args 命令参数，每一项可以为字符串或是字符串数组
+ * @example
+ * execSync('rm', '-rf', 'node_modules')
+ * execSync('git clone', 'xxx')
+ */
+export function justExecSync(...args: Array<string>) {
+    const command = args.join(' ');
     const result = spawnSync(command, { shell: true, stdio: 'inherit' });
     if (result.status) {
         logger.error(String(result.stderr || result.stdout));
